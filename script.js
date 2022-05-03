7b4b8a4e47ef1c0f927eb3c3d1372471f5ad49c3
Junjie portátil <jzhou@institutmvm.cat>
2022-05-03 15:37:25 +0200

    Código completado

---
+++ script.js
@@ -0,0 +1,31 @@
+function section (character){
+    //template
+    return `<div class="example-1 card">
+    <div class="wrapper" style="background: url(./img/${character.name}.webp) 50% 1%/cover">
+      <div class="date">
+        <span class="day">${character.birthday}</span>
+      </div>
+      <div class="data">
+        <div class="content">
+          <span class="author">${character.vision}</span>
+          <h1 class="title"><a href="#">${character.name}</a></h1>
+          <p class="text">${character.description}</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+}
+
+async function render(){
+    $.getJSON("https://genshinlist.com/api/characters", function(result){
+        let html = '';
+        $.each(result, function(i, field){
+            console.log(field);
+            html += section(field);
+
+            let container = document.querySelector('.row')
+            container.innerHTML = html;
+        })
+    })
+}
+render();
